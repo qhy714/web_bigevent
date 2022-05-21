8056b634323c1f77dc79a66348e7b8ff754da595
qhy714 <192390664@qq.com>
2022-05-21 10:18:40 +0800

    完成了首页

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -2,5 +2,23 @@
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
 // 请求拦截器
 $.ajaxPrefilter((options) => {
-  options.url = `http://www.liulongbin.top:3007`+options.url;
-})
+  options.url = `http://www.liulongbin.top:3007` + options.url;
+  // 在请求之前给有权限的接口注入token
+  if (options.url.includes('/my/')) {
+    options.headers = {
+      Authorization: localStorage.getItem('token'),
+    };
+  }
+  options.complete = (res) => {
+    console.log(res);
+    if (
+      res.responseJSON.status === 1 &&
+      res.responseJSON.message === '身份认证失败！'
+    ) {
+      // console.log(1);
+      // 清空 token
+      localStorage.removeItem('token');
+      location.href = '/login.html';
+    }
+  };
+});
